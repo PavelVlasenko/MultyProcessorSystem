5a5e42bcbf6202dc4baa1984d8115b7eefa807f3
PV <pavel-developer@yandex.ru>
2017-03-19 15:13:14 +0700

    cloud data

--- src/multyproc/Config.java
+++ src/multyproc/Config.java
@@ -2,7 +2,7 @@ package multyproc;
 
 public class Config {
     public static int processorCount = 3;
-    public static String cloudDataPath = "C:\\Users\\SBT-Vlasenko-PV\\Up\\test\\cloud3";
+    public static String cloudDataPath = "C:\\Users\\LLNL-Atlas-2006-0";
     public static int taskCount = 64;
     public static int executionTimeDelimeter = 300;
     public static int arrivalTimeGap = 100;
